55e60df598228124a37569718f931f9cb03dbeab
kat <kat@gmail.com>
2025-06-01 03:55:25 +0800

    removed loading in root index

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,28 +4,17 @@ import { Stack } from "expo-router"
 import { useEffect, useState } from "react"
 import { onAuthStateChanged, type User } from "firebase/auth"
 import { auth } from "../config/firebase"
-import { View, Text } from "react-native"
 
 export default function RootLayout() {
   const [user, setUser] = useState<User | null>(null)
-  const [isLoading, setIsLoading] = useState(true)
 
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, (user) => {
       setUser(user)
-      setIsLoading(false)
     })
 
     return unsubscribe
   }, [])
-
-  if (isLoading) {
-    return (
-      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-        <Text>Loading...</Text>
-      </View>
-    )
-  }
 
   return (
     <Stack screenOptions={{ headerShown: false }}>
